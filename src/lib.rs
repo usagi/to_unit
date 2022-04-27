8468f44e225db4d673e87f877d06ed18e6c5506a
Usagi Ito <the@usagi.network>
2022-04-27 20:29:43 +0900

    __

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,20 @@
+/// Any type -> ()
 pub trait ToUnit
 {
+ /// Any type -> ()
  fn to_unit(&self) {}
 }
 
+/// Any type -> ()
 impl<T> ToUnit for T {}
+
+/// Any type -> ()
+/// Same to `to_unit::ToUnit`, but it's type more easily.
+pub trait __
+{
+ /// Any type -> ()
+ fn __(&self) {}
+}
+
+/// Any type -> ()
+impl<T> __ for T {}
--- tests/test.rs
+++ tests/test.rs
@@ -1,9 +1,8 @@
-use to_unit::ToUnit;
-// or, `use to_unit::ToUnit as __` for ultra lzay persons!
-
 #[test]
 fn match_arms_to_easily()
 {
+ use to_unit::ToUnit; // <-- here
+
  use std::collections::HashMap;
  let mut x = HashMap::<String, i32>::new();
  x.insert("neko".to_string(), 123);
@@ -20,4 +19,20 @@ fn match_arms_to_easily()
  //      }
  // I don't like the multi-line behaviors, so I made the __ lib.
  //
+}
+
+/// Or, `use to_unit::__` for ultra lazy persons!
+#[test]
+fn for_ultra_lazy_persons()
+{
+ use to_unit::__; // <-- here
+
+ use std::collections::HashMap;
+ let mut x = HashMap::<String, i32>::new();
+ x.insert("neko".to_string(), 123);
+ match x.get_mut("neko")
+ {
+  Some(v) => *v = 222,
+  None => x.insert("neko".to_string(), 222).__() // <-- here!
+ }
 }
