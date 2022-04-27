3f5dc6877c218f26ccc35917d6a3769a7ebd8630
Usagi Ito <the@usagi.network>
2022-04-27 12:38:17 +0900

    1.0.0

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub trait __
+{
+ fn __(&self) {}
+}
+
+impl<T> __ for T {}
---
+++ tests/test.rs
@@ -0,0 +1,22 @@
+use __::__;
+
+#[test]
+fn match_arms_to_easily()
+{
+ use std::collections::HashMap;
+ let mut x = HashMap::<String, i32>::new();
+ x.insert("neko".to_string(), 123);
+ match x.get_mut("neko")
+ {
+  Some(v) => *v = 222,
+  None => x.insert("neko".to_string(), 222).__() // <-- here!
+ }
+ // Ofcorse alternatively, you can write:
+ //  eg. None => { x.insert("neko".to_string(), 222); }
+ // But, it might be fix to a multi-line format by rust-fmt then...:
+ //  eg. None => {
+ //          x.insert("neko".to_string(), 222);
+ //      }
+ // I don't like the multi-line behaviors, so I made the __ lib.
+ //
+}
